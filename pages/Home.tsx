d2ac2f7ebc9364a7ea1546c23253c987d2e0f3cf
Ydannel <44483665+Ydannel@users.noreply.github.com>
2022-11-22 16:48:39 -0400

    +page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,7 +1,10 @@
+import { PageWrapper } from "../components"
 
 export default function Home() {
   return (
-    <>
+    <PageWrapper>
+    
+          <>
       <section className="dark:bg-gray-800 dark:text-gray-100">
         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
           <div className="flex items-center justify-center p-40  mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
@@ -62,7 +65,52 @@ export default function Home() {
       </div>
       
       </div>
-</section>
+      </section>
+
+      <section>
+       <div className="md:mx-auto md:container px-4">
+                <div className="pt-10 md:pt-40">
+                    <div className="container mx-auto">
+                        <div className="flex flex-wrap items-center pb-12">
+                            <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
+                                <div className="py-2 text-color">
+                                    <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black dark:text-gray-800">Lorem ipsum dolor</h1>
+                                    <h2 className="text-lg lg:text-xl lg:leading-7 md:leading-10 f-f-r py-4 md:py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione dolorum voluptatibus perspiciatis assumenda! Ea amet ipsum, ab magni nemo corporis odit tenetur. Commodi tenetur, in provident facere facilis dolorem!</h2>
+                                    <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                                        <a  href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md mr-20" src="../resourses/img/2.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="pb-32 pt-16">
+                    <div className="mx-auto">
+                        <div className="flex flex-wrap flex-row-reverse items-center">
+                            <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
+                                <div className="py-2 text-color">
+                                    <h1 className="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black dark:text-gray-800">Lorem impusn</h1>
+                                    <h2 className="text-lg lg:text-xl  leading-7 md:leading-10 f-f-r py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam accusamus atque vero tenetur cum sint, sapiente adipisci delectus consequatur pariatur temporibus praesentium sequi repellat doloremque facilis labore ratione dolorem ipsum?</h2>
+                                    <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                                        <a  href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
+                                  
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="../resourses/img/3.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
     </>
+    
+    </PageWrapper>
+
   );
 }
