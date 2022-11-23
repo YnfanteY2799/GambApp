379bde2fd61e0d08228c9c08b811ebe9919fc02c
Ydannel <44483665+Ydannel@users.noreply.github.com>
2022-11-23 11:53:11 -0400

    +contact

--- components/continuous/Footer.tsx
+++ components/continuous/Footer.tsx
@@ -1,14 +1,78 @@
 export default function Footer() {
   return (
-    <footer className="p-4 text-gray-800 bg-gray-300 bottom footer footer-center ">
-      <div className="text-center">
-        <p>
-          Copyright © 2022 -
-          <a className="font-semibold" href="mailto:m.sohanemon@gmail.com">
-            SohanEmon
-          </a>
-        </p>
-      </div>
-    </footer>
+ 
+<div className=" bg-gray-800">
+    <div className="max-w-2xl mx-auto text-white py-10">
+        <div className="text-center">
+            <h3 className="text-3xl mb-3"> LOGO HERE </h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae natus assumenda dolorem, nesciunt ipsam deserunt  </p>
+            <div className="flex justify-center my-10">
+                <div className="flex justify-center ">
+                    <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                        <svg aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fab"
+                        data-icon="facebook-f"
+                        className="w-2 h-full mx-auto"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                        ></path>
+                        </svg>
+                      </a>
+                </div>
+                <div className="flex justify-center">
+                  
+                      <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                        <svg aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fab"
+                          data-icon="twitter"
+                          className="w-3 h-full mx-auto"
+                          role="img"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 512 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+                          ></path>
+                        </svg>
+                      </a>
+                </div>
+                    <div className="flex justify-center ">
+                    <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                        <svg aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fab"
+                        data-icon="instagram"
+                        className="w-2 h-full mx-auto"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                        ></path>
+                        </svg>
+                      </a>
+                </div>
+            </div>
+        </div>
+        <div className="mt-20 flex flex-col md:flex-row md:justify-between items-center text-sm  text-gray-400">
+            <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; Copyright 2022. </p>
+            <div className="order-1 md:order-2">
+                <span className="px-2">Nosotros</span>
+                <span className="px-2 border-l">Contáctanos</span>
+                <span className="px-2 border-l">Términos</span>
+            </div>
+        </div>
+    </div>
+</div>
   );
 }
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -3,113 +3,113 @@ import { PageWrapper } from "../components"
 export default function Home() {
   return (
     <PageWrapper>
-    
-          <>
-      <section className="dark:bg-gray-800 dark:text-gray-100">
-        <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-          <div className="flex items-center justify-center p-40  mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-            <img src="../resourses/img/1.png" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+
+      <>
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+          <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between m:justify-between sm:justify-between">
+            <div className="flex items-center justify-center p-40  mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 ">
+              <img src="../resourses/img/1.png" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+            </div>
+            <div className="flex flex-col justify-center -pl-40 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+              <h1 className="text-5xl font-bold leading-none sm:text-6xl">lorem
+                <span className="dark:text-red-400">senectus</span> erat pharetra
+              </h1>
+              <p className="mt-6 mb-8 text-lg sm:mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <br className="hidden md:inline lg:hidden" />Sed ducimus labore possimus eveniet voluptate illo s
+              </p>
+              <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                <a href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
+                <a href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100">Apuesta en vivo</a>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col justify-center -pl-40 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className="text-5xl font-bold leading-none sm:text-6xl">lorem
-              <span className="dark:text-red-400">senectus</span> erat pharetra
-            </h1>
-            <p className="mt-6 mb-8 text-lg sm:mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              <br className="hidden md:inline lg:hidden"/>Sed ducimus labore possimus eveniet voluptate illo s
-            </p>
-            <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-              <a  href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
-              <a  href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100">Apuesta en vivo</a>
+        </section>
+
+        <section>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1f2937" fill-opacity="1" d="M0,224L72,96L144,64L216,288L288,128L360,128L432,0L504,192L576,64L648,0L720,32L792,32L864,192L936,224L1008,288L1080,128L1152,96L1224,160L1296,224L1368,320L1440,192L1440,0L1368,0L1296,0L1224,0L1152,0L1080,0L1008,0L936,0L864,0L792,0L720,0L648,0L576,0L504,0L432,0L360,0L288,0L216,0L144,0L72,0L0,0Z"></path></svg>
+        </section>
+
+        <section className="">
+          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-4 sm:py-1 lg:px-8">
+            <div className="mt-16 grid gap-8 sm:grid-cols-3  lg:grid-cols-3 md:grid-cols-3">
+              <div>
+                <img
+                  alt="User"
+                  src="../resourses/icons/user.svg"
+                  className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
+                />
+                <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
+                  <p className="text-lg font-bold text-gray-700">Lorem</p>
+                </div>
+              </div>
+              <div>
+                <img
+                  alt="Dollar"
+                  src="../resourses/icons/dollar-sign.svg"
+                  className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
+                />
+                <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
+                  <p className="text-lg font-bold text-gray-700">Impusm</p>
+                </div>
+              </div>
+              <div>
+                <img
+                  alt="Stars"
+                  src="../resourses/icons/star.svg"
+                  className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
+                />
+                <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
+                  <p className="text-lg font-bold text-gray-700">Dolor</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </section>
+
+        <section>
+          <div className="md:mx-auto md:container px-4">
+            <div className="pt-10 md:pt-40">
+              <div className="container mx-auto">
+                <div className="flex flex-wrap items-center pb-12">
+                  <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
+                    <div className="py-2 text-color">
+                      <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black dark:text-gray-800">Lorem ipsum dolor</h1>
+                      <h2 className="text-lg lg:text-xl lg:leading-7 md:leading-10 f-f-r py-4 md:py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione dolorum voluptatibus perspiciatis assumenda! Ea amet ipsum, ab magni nemo corporis odit tenetur. Commodi tenetur, in provident facere facilis dolorem!</h2>
+                      <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                        <a href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-blue-400 dark:text-gray-900">Registrate</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                    <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md mr-20" src="../resourses/img/2.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="pb-32 pt-16">
+              <div className="mx-auto">
+                <div className="flex flex-wrap flex-row-reverse items-center">
+                  <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
+                    <div className="py-2 text-color">
+                      <h1 className="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black dark:text-gray-800">Lorem impusn</h1>
+                      <h2 className="text-lg lg:text-xl  leading-7 md:leading-10 f-f-r py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam accusamus atque vero tenetur cum sint, sapiente adipisci delectus consequatur pariatur temporibus praesentium sequi repellat doloremque facilis labore ratione dolorem ipsum?</h2>
+                      <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                        <a href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-blue-400 dark:text-gray-900">Registrate</a>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                    <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="../resourses/img/3.png" />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </>
 
-      <section>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1f2937" fill-opacity="1" d="M0,224L72,96L144,64L216,288L288,128L360,128L432,0L504,192L576,64L648,0L720,32L792,32L864,192L936,224L1008,288L1080,128L1152,96L1224,160L1296,224L1368,320L1440,192L1440,0L1368,0L1296,0L1224,0L1152,0L1080,0L1008,0L936,0L864,0L792,0L720,0L648,0L576,0L504,0L432,0L360,0L288,0L216,0L144,0L72,0L0,0Z"></path></svg>
-      </section>
-
-      <section className="">
-        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-1 lg:px-8">
-          <div className="mt-16 grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-16 lg:grid-cols-3">
-            <div>
-              <img
-                alt="User"
-                src="../resourses/icons/user.svg"
-                className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-              />
-              <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
-                <p className="text-lg font-bold text-gray-700">Lorem</p>
-              </div>
-            </div>
-                 <div>
-              <img
-                alt="Dollar"
-                src="../resourses/icons/dollar-sign.svg"
-                className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-              />
-              <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
-                <p className="text-lg font-bold text-gray-700">Impusm</p>
-              </div>
-            </div>
-                 <div>
-              <img
-                alt="Stars"
-                src="../resourses/icons/star.svg"
-                className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-              />
-              <div className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center">
-                <p className="text-lg font-bold text-gray-700">Dolor</p>
-              </div>
-            </div>
-      </div>
-      
-      </div>
-      </section>
-
-      <section>
-       <div className="md:mx-auto md:container px-4">
-                <div className="pt-10 md:pt-40">
-                    <div className="container mx-auto">
-                        <div className="flex flex-wrap items-center pb-12">
-                            <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
-                                <div className="py-2 text-color">
-                                    <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black dark:text-gray-800">Lorem ipsum dolor</h1>
-                                    <h2 className="text-lg lg:text-xl lg:leading-7 md:leading-10 f-f-r py-4 md:py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione dolorum voluptatibus perspiciatis assumenda! Ea amet ipsum, ab magni nemo corporis odit tenetur. Commodi tenetur, in provident facere facilis dolorem!</h2>
-                                    <div className="flex items-center cursor-pointer pb-4 md:pb-0">
-                                        <a  href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md mr-20" src="../resourses/img/2.png" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="pb-32 pt-16">
-                    <div className="mx-auto">
-                        <div className="flex flex-wrap flex-row-reverse items-center">
-                            <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
-                                <div className="py-2 text-color">
-                                    <h1 className="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black dark:text-gray-800">Lorem impusn</h1>
-                                    <h2 className="text-lg lg:text-xl  leading-7 md:leading-10 f-f-r py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam accusamus atque vero tenetur cum sint, sapiente adipisci delectus consequatur pariatur temporibus praesentium sequi repellat doloremque facilis labore ratione dolorem ipsum?</h2>
-                                    <div className="flex items-center cursor-pointer pb-4 md:pb-0">
-                                        <a  href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-red-400 dark:text-gray-900">Registrate</a>
-                                  
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="../resourses/img/3.png" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-      </section>
-    </>
-    
     </PageWrapper>
 
   );
